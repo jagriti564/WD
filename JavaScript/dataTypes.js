36df6df0973dad30f63e6886c716b1d8b4c2374f
jagriti564 <singhjagriti2004@gmail.com>
2024-06-24 18:49:50 +0530

    Variable and datatype in javascript

---
+++ JavaScript/dataTypes.js
@@ -0,0 +1,9 @@
+let a = null;
+let b = 7; // number
+let c =  "Hello World" // String
+let d = Symbol("$") // Symbol
+let e = true //boolen
+let f = BigInt("514") // BigInt
+let g = undefined; // undefined
+
+console.log(a,b,c,d,e,f,g);
---
+++ JavaScript/index.js
@@ -0,0 +1,11 @@
+// var a = 7;
+// var a = 5;
+
+// let a = 7;
+// a = 5;
+// console.log(a);
+
+
+// const a = 3.14;
+// console.log(a);
+
