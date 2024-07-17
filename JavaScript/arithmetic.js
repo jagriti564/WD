a4612b2295d751b069597c0a2a2a8ae1f036d43a
jagriti564 <singhjagriti2004@gmail.com>
2024-07-17 18:52:22 +0530

    Arithmetic operoters

---
+++ JavaScript/arithmetic.js
@@ -0,0 +1,22 @@
+let a = 4
+let b = 5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a**b);
+// console.log(a%b);
+// console.log(a++);
+// console.log(--a);
+console.log(a++); //4
+console.log(a++); //5
+console.log(--a); //5
+console.log(a--); //5
+console.log(a++); //4
+console.log(a++); //5
+console.log(--a); //5
+console.log(--a); //4
+console.log(a); //4
+console.log(--a); //3
+console.log(a); //3
