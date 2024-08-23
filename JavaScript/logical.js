1649782f9edf47d09ea8dc77fb409405487cfea1
jagriti564 <singhjagriti2004@gmail.com>
2024-08-23 18:52:31 +0530

    string

--- JavaScript/logical.js
+++ JavaScript/logical.js
@@ -1,6 +1,6 @@
 let x = 6;
 let y = 7;
 
-// console.log(x==6 && y==7);
-// console.log(x==6 || y==5);
-// console.log(x==6 != y==5);
+console.log(x==6 && y==7);
+console.log(x==6 || y==5);
+console.log(x==6 != y==5);
---
+++ JavaScript/string.js
@@ -0,0 +1,18 @@
+// let a = "Jagriti"
+// let b = 7
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+
+
+// let a = "ABCDEFGHIJKMOPQRSTUVWXYZ"
+// console.log(a.length);
+
+// let a = "jagriti"
+// console.log(a.charAt(1));
+
+// let a = "jaGriti"
+// console.log(a.charCodeAt(2));
+
+let a = "jaGriti"
+console.log(a.charCodeAt(2));
