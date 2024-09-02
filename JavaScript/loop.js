4a114c46b8983380546e331e696a3b10f56caf8e
jagriti564 <singhjagriti2004@gmail.com>
2024-09-02 18:53:57 +0530

    complete switch and loop

---
+++ JavaScript/loop.js
@@ -0,0 +1,27 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// for(i = 0; i<=1000; i++){
+//     console.log(i);
+// }
+
+// let i = 0;
+// while(i<10){
+//     console.log(i);
+//     i++
+// }
+
+let i = 0;
+do{
+    console.log(i);
+    i++
+}
+while(i>10)
---
+++ JavaScript/switch.js
@@ -0,0 +1,23 @@
+let d = new Date();
+let day = d.getDay()
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thrusday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+}
