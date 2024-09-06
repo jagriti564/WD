c42fd7187c8c4bdd223d9d930a7fd0138d6c8470
jagriti564 <singhjagriti2004@gmail.com>
2024-09-06 18:49:35 +0530

    array method part 1 complete

---
+++ JavaScript/array.js
@@ -0,0 +1,35 @@
+// const jagritiFriendList = ["Sona","Mona","Kajal","Khushi"]
+// console.log(jagritiFriendList[0]);
+
+// Array Lenght
+// const txt = ["A","B","C","D","E","F","G"]
+// console.log(txt.length);
+
+
+// const txt = ["A","B","C","D","E","F","G"]
+// let newTxt = txt.toString()
+// console.log(newTxt);
+
+// Join
+// const txt = ["A","B","C","D","E","F","G"]
+// console.log(txt.join("-"));
+
+// const number = [1,2,3,4,5,6,7,8,9]
+// const newNumber = number.pop()
+// const newNumber1 = number.pop()
+// console.log(newNumber1);
+// console.log(number);
+
+// const number = [1,2,3,4,5,6,7,8,9]
+// number.push(10) 
+// number.push(11) 
+// console.log(number);
+
+// const number = [1,2,3,4,5,6,7,8,9]
+// number.shift()
+// console.log(number);
+
+const number = [1,2,3,4,5,6,7,8,9]
+number.unshift(0)
+console.log(number);
+
